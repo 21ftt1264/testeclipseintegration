f1a2bf5258c4c89f8a15e318fe981dd22976e1e6
Arieen <21ftt1264@student.pb.edu.bn>
2023-01-31 14:48:04 +0800

    first commit from me

---
+++ testeclipseintegration/src/testeclipseintegration/TestNep/TestNep.java
@@ -0,0 +1,12 @@
+package testeclipseintegration.TestNep;
+
+public class TestNep {
+	public static void main(String[] args) {
+		  double interval = 2.5;
+		    for (double i = 1; i <= 100; i = i + interval) {
+		      System.out.println(i);
+		    }
+	}
+
+}
+
